1b753d90ebd8ead63c94c068f0e94abe7371f8bf
Rafael <RafaelB24@yahoo.com>
2019-08-31 21:42:14 -0400

    Add model test to prevent duplicate usernames and passwords less than 5 characters

--- guitar-lists-api/db/schema.rb
+++ guitar-lists-api/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_010147) do
+ActiveRecord::Schema.define(version: 2019_09_01_011628) do
+
+  create_table "guitars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sellers", force: :cascade do |t|
     t.datetime "created_at", null: false
--- guitar-lists-api/spec/models/seller_spec.rb
+++ guitar-lists-api/spec/models/seller_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Seller, type: :model do
   describe 'validations' do
+    # Shoulda-Matcher validations
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password) }
     it { should have_secure_password }
     it { should have_many(:guitars) }
+
+    it 'Should not create a username if it already exists' do
+      User.create(username: 'first', password: 'abc123')
+      second = User.create(username: 'first', password: 'abc123')
+
+      expect(second.valid?).to eq(false)
+    end
+
+    it 'Should not create a username with a password less than 5 characters' do
+      user = User.create(username: 'first', password: 'abc')
+
+      expect(user.valid?).to eq(false)
+    end
   end
 end
