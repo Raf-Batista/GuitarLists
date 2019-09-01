61e8f232124a23f82dce80a1932ac1f4d56f77ba
Rafael <RafaelB24@yahoo.com>
2019-08-31 21:26:34 -0400

    Create Guitar model, add model validation tests for Guitar

--- guitar-lists-api/spec/models/seller_spec.rb
+++ guitar-lists-api/spec/models/seller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Seller, type: :model do
   describe 'validations' do
-    it { should have_many(:guitars) }
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password) }
     it { should have_secure_password }
+    it { should have_many(:guitars) }
   end
 end
