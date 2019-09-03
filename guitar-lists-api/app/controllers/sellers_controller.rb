979ff0045cf4e16503602fa8665dfc1453de80f0
Rafael <RafaelB24@yahoo.com>
2019-09-02 22:53:42 -0400

    Change Sellers #index to be more readable, update Seller request tests to return guitars with JSON data

--- guitar-lists-api/app/controllers/sellers_controller.rb
+++ guitar-lists-api/app/controllers/sellers_controller.rb
@@ -3,6 +3,17 @@ class SellersController < ApplicationController
   def index
     @sellers = Seller.all
 
-    render json: @sellers.to_json(except: [:created_at, :updated_at, :password_digest]), status: 200
+    render json: @sellers.to_json(
+      include: {
+        #JSON will return guitars that belong to sellers
+        guitars: {
+          #JSON will not return these attributes for guitars
+          except: [:created_at, :updated_at]
+          }
+        },
+        #JSON will not return these attributes for sellers
+        except: [:created_at, :updated_at, :password_digest]
+        ),
+      status: 200
   end
 end
--- guitar-lists-api/spec/controllers/sellers_controller_spec.rb
+++ guitar-lists-api/spec/controllers/sellers_controller_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe SellersController, type: :request do
 
     describe "Get #index" do
-      let!(:sellers) { 3.times{|index| Seller.create(username: "test#{index+1}", password: "test123")} }
+      let!(:sellers) do
+        3.times do |index|
+          seller = Seller.create(username: "test#{index+1}", password: "test123")
+          seller.guitars.build(model: "test-model#{index+1}", spec: "test-specs", price: 5, condition: "new", location: "somewhere").save
+          seller.guitars.build(model: "test-model#{index+10}", spec: "test-specs", price: 5, condition: "new", location: "somewhere").save
+        end
+      end
+
       before(:example) { get '/sellers' }
 
       it 'returns HTTP Success' do
@@ -17,7 +24,7 @@ RSpec.describe SellersController, type: :request do
 
       it 'should return with the correct keys' do
         json_response = JSON.parse(response.body)
-        expect(json_response[0].keys).to match(["id", "username"])
+        expect(json_response[0].keys).to match(["id", "username", "guitars"])
       end
 
       it 'should not return password_digest, created_at, and updated_at' do
@@ -27,12 +34,12 @@ RSpec.describe SellersController, type: :request do
 
       it 'should return an array of guitars' do
         json_response = JSON.parse(response.body)
-        expect(json_response[0][:guitars].class).to eq('Array')
+        expect(json_response[0]["guitars"].class).to eq(Array)
       end
 
       it 'should return correct number of guitars' do
         json_response = JSON.parse(response.body)
-        expect(json_response[0][:guitars].size).to eq(2)
+        expect(json_response[0]["guitars"].size).to eq(2)
       end
 
     end
