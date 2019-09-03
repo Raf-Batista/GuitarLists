16ae7884a2937b45ee2a13ffffc23115cfad8bb8
Rafael <RafaelB24@yahoo.com>
2019-09-02 23:01:43 -0400

    Create Guitars controller, Add tests for Guitars #index request

---
+++ guitar-lists-api/spec/controllers/guitars_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe GuitarsController, type: :request do
+    describe 'Get #index' do
+      let!(:sellers) do
+        3.times do |index|
+          seller = Seller.create(username: "test#{index+1}", password: "test123")
+          seller.guitars.build(model: "test-model#{index+1}", spec: "test-specs", price: 5, condition: "new", location: "somewhere").save
+          seller.guitars.build(model: "test-model#{index+10}", spec: "test-specs", price: 5, condition: "new", location: "somewhere").save
+        end
+      end
+
+      before(:example) { get '/guitars' }
+
+      it 'returns HTTP success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'should returns correct number of guitars' do
+        json_response = JSON.parse(response.body)
+        expect(json_response.size).to eq(3)
+      end
+
+      it 'should only returns correct attributes' do
+        json_response = JSON.parse(response.body)
+        expect(json_response[0].keys).to_not match["created_at", "updated_at"]
+      end
+    end
+end
