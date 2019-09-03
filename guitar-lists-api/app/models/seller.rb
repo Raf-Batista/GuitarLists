ca2c8c6a3b63cb3ee0497d3c95bf9ff52b4bb5a4
Rafael <RafaelB24@yahoo.com>
2019-09-02 21:16:05 -0400

    Create Guitar model, add attributes to Guitar

--- guitar-lists-api/app/models/seller.rb
+++ guitar-lists-api/app/models/seller.rb
@@ -3,5 +3,6 @@ class Seller < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :password, length: { minimum: 5, too_short: "Password should be %{count} or more characters" }
+  has_many :guitars
   has_secure_password
 end
