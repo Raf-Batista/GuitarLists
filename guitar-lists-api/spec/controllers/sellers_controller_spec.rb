44fa1ed5aeeb974fc4333543d24d4935fa44edb1
Rafael <RafaelB24@yahoo.com>
2019-09-03 23:35:48 -0400

    Add Guitars #show tests

--- guitar-lists-api/spec/controllers/sellers_controller_spec.rb
+++ guitar-lists-api/spec/controllers/sellers_controller_spec.rb
@@ -41,7 +41,36 @@ RSpec.describe SellersController, type: :request do
         json_response = JSON.parse(response.body)
         expect(json_response[0]["guitars"].size).to eq(2)
       end
+  end
 
+  describe '#show'do
+  let!(:seller) do
+    seller = Seller.create(username: "test", password: "test123")
+    seller.guitars.build(model: "test-model-1", spec: "test-specs", price: 5, condition: "new", location: "somewhere").save
+    seller.guitars.build(model: "test-model-2", spec: "test-specs", price: 5, condition: "new", location: "somewhere").save
+  end
+
+    it 'returns HTTP success' do
+      # spec didn't work using symbols with params
+      # get :show, params: {id: Seller.first.id}
+      get "http://localhost:3000/sellers/#{Seller.first.id}"
+      excpect(response).to have_http_status(:success)
     end
 
+    it 'does not return created_at and updated_at' do
+      get "http://localhost:3000/sellers/#{Seller.first.id}"
+      json_response = JSON.parse(response.body)
+      expect(json_response.keys).to not_match(["created_at", "updated_at"])
+    end
+
+    it 'returns an array of guitars' do
+      get "http://localhost:3000/sellers/#{Seller.first.id}"
+      expect(json_response["guitars"].class).to eq(Array)
+    end
+
+    it 'returns an correct number of guitars' do
+      get "http://localhost:3000/sellers/#{Seller.first.id}"
+      expect(json_response["guitars"].size).to eq(2)
+    end
+  end
 end
