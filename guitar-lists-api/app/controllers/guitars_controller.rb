f0da9a2be975a089d7d74eb55708f3c2c554db4f
Rafael <RafaelB24@yahoo.com>
2019-09-05 18:42:46 -0400

    Add #update and private#guitar_params to GuitarsController

--- guitar-lists-api/app/controllers/guitars_controller.rb
+++ guitar-lists-api/app/controllers/guitars_controller.rb
@@ -9,4 +9,14 @@ class GuitarsController < ApplicationController
     render json: @guitar.to_json(except: [:created_at, :updated_at])
   end
 
+  def update
+    @guitar = Guitar.find(params[:id])
+    @guitar.update(guitar_params)
+  end
+
+  private
+
+  def guitar_params
+    params.require(:guitar).permit(:model, :spec, :price, :condition, :location)
+  end
 end
