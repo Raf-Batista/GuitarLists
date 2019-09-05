0718adf2b0a2116e02ec2523acf86bfb4a84ad42
Rafael <RafaelB24@yahoo.com>
2019-09-05 12:34:22 -0400

    Add if with a Proc for password validation that will not update password if left blank, Sellers controller updates username and password in #update

--- guitar-lists-api/app/models/seller.rb
+++ guitar-lists-api/app/models/seller.rb
@@ -1,8 +1,9 @@
 class Seller < ApplicationRecord
 
-  validates :username, presence: true
-  validates :username, uniqueness: { message: 'Username has already been taken. Please login or choose a different username' }
-  validates :password, length: { minimum: 5, too_short: "Password should be %{count} or more characters" }, on: :create
+  validates :username, presence: true, uniqueness: { message: 'Username has already been taken. Please login or choose a different username' }
+  # when using #sellers_params in SellersController #update, update will return false. Passing 'if' with a proc, validate password length if
+    # the seller is a new record or the password is not blank. If it is blank it will still return true when updating but password will not be changed
+  validates :password, length: { minimum: 5, too_short: "Password should be %{count} or more characters" }, if: Proc.new{ |obj| obj.new_record? || !obj.password.blank? }
   has_many :guitars
   has_secure_password
 end
