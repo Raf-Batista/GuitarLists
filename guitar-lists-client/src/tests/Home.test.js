5f20d9e7645beab64e41ee9ad069dd241f28ed20
Rafael <RafaelB24@yahoo.com>
2019-09-06 22:35:56 -0400

    Change home.js and home.test.js to Home, change description of test in Home.test.js

--- guitar-lists-client/src/tests/Home.test.js
+++ guitar-lists-client/src/tests/Home.test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
-describe('H1 Text', () => {
-  test('h1 loads correctly', async () => {
+describe('renders home page correctly', () => {
+  test('site title loads correcly', async () => {
 	let browser = await puppeteer.launch({
 	  headless: false
 	});
