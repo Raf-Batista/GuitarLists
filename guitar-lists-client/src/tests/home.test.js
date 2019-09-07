d464f62b45fa3fe03650ecff026ee12270390bac
Rafael <RafaelB24@yahoo.com>
2019-09-06 22:28:03 -0400

    Add test to check if GuitarLists is rendered

---
+++ guitar-lists-client/src/tests/home.test.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+describe('H1 Text', () => {
+  test('h1 loads correctly', async () => {
+	let browser = await puppeteer.launch({
+	  headless: false
+	});
+	let page = await browser.newPage();
+
+	page.emulate({
+	  viewport: {
+		width: 500,
+		height: 2400
+	  },
+	  userAgent: ''
+	});
+
+	await page.goto('http://localhost:3001/');
+	await page.waitForSelector('.App-title');
+
+	const html = await page.$eval('.title', e => e.innerHTML);
+	expect(html).toBe('Welcome to GuitarLists!');
+
+	browser.close();
+  }, 16000);
+});
