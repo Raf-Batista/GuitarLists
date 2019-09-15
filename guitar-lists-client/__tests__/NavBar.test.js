3bf99bb50aa362c930c4980f21704f765681fd4e
Rafael <RafaelB24@yahoo.com>
2019-09-15 17:10:09 -0400

    Change test name

--- guitar-lists-client/__tests__/NavBar.test.js
+++ guitar-lists-client/__tests__/NavBar.test.js
@@ -13,7 +13,7 @@ describe(
       await page.close()
     })
 
-    it('should contain working links', async () => {
+    it('should go to about page from home page', async () => {
       await Promise.all([page.click('a.about'), page.waitForNavigation()]);
       await expect(page.url()).toMatch('http://localhost:3001/about')
     })
