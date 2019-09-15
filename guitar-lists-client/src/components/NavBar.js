323a3d19d34924905d5636dc0a39e3a5d97172a2
Rafael <RafaelB24@yahoo.com>
2019-09-15 16:20:08 -0400

    Add bootstrap styling to NavBar

--- guitar-lists-client/src/components/NavBar.js
+++ guitar-lists-client/src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+
 class NavBar extends Component {
   render(){
     return(
       <div>
-        <NavLink exact activeClassName = 'active-link' className = 'home' to='/'>
-          Home
+        <nav class="navbar navbar-dark bg-dark">
+        <NavLink exact activeClassName = 'active-link' className = 'home navbar-brand' to='/'>
+          <i class="fas fa-guitar"></i>
+          GuitarLists
         </NavLink>
         <NavLink exact activeClassName = 'active-link' className = 'about' to='/about'>
           About
-        </NavLink>
+        </NavLink>''
+        </nav>
       </div>
     )
   }
