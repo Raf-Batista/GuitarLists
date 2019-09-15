69c3dfa6a1eaf67a7f03115b87b2ed284a8c525b
Rafael <RafaelB24@yahoo.com>
2019-09-15 16:44:52 -0400

    Add auto margin right to about link

--- guitar-lists-client/src/components/NavBar.js
+++ guitar-lists-client/src/components/NavBar.js
@@ -5,14 +5,18 @@ class NavBar extends Component {
   render(){
     return(
       <div>
-        <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <NavLink exact activeClassName = 'active-link' className = 'home navbar-brand' to='/'>
           <i class="fas fa-guitar"></i>
           GuitarLists
         </NavLink>
-        <NavLink exact activeClassName = 'active-link' className = 'about' to='/about'>
-          About
-        </NavLink>''
+        <ul class="navbar-nav mr-auto">
+          <li>
+            <NavLink exact activeClassName = 'active-link' className = 'about' to='/about'>
+              About
+            </NavLink>
+          </li>
+        </ul>
         </nav>
       </div>
     )
