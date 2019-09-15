9d518b751a25279a0cc51d6b9e2aed99beb153d1
Rafael <RafaelB24@yahoo.com>
2019-09-14 21:24:20 -0400

    Add about route to App.js, create components/About.js

--- guitar-lists-client/src/App.js
+++ guitar-lists-client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import Home from './components/Home';
+import About from './components/About'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <NavBar />
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
       </Switch>
     </div>
   );
