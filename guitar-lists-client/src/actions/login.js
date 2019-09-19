5fd511445ecd11003b7de59bd4327aedf931f5a5
Rafael <RafaelB24@yahoo.com>
2019-09-18 23:39:53 -0400

    Add fetch to actions/login.js

--- guitar-lists-client/src/actions/login.js
+++ guitar-lists-client/src/actions/login.js
@@ -1,5 +1,16 @@
-const login = () => {
-
+const login = (sellerInfo) => {
+  return dispatch => {
+    return fetch('http://localhost:3001/login', {
+      method: 'POST',
+      body: JSON.stringify(sellerInfo),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
 }
 
 export default login
