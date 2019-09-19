d427d3cfceb8dfd66cb1d2f27ec98ab8c63b5158
Rafael <RafaelB24@yahoo.com>
2019-09-19 00:08:54 -0400

    Add form to Home

--- guitar-lists-client/src/components/Home.js
+++ guitar-lists-client/src/components/Home.js
@@ -5,6 +5,17 @@ export default class Home extends Component {
     return(
       <div>
         <h1>Welcome to GuitarLists</h1>
+        <div>
+          <form>
+            <label name="username">Username</label>
+            <input type="text" name="usernmame" />
+
+            <label name="password">Password</label>
+            <input type="password" name="password" />
+
+            <input type="submit" />
+          </form>
+        </div>
       </div>
     )
   }
