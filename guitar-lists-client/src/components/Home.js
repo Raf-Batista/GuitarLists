0d58dffe6341a737fa1fa3cad3ec6c881f1a4680
Rafael <RafaelB24@yahoo.com>
2019-09-19 22:35:54 -0400

    Add handleOnchange and handleOnSubmit

--- guitar-lists-client/src/components/Home.js
+++ guitar-lists-client/src/components/Home.js
@@ -4,26 +4,38 @@ import signup from '../actions/signup'
 export default class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = {username: '', password: ''}
+    this.state = { username: '', password: '' }
   }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+ }
+
+  handleOnSubmit = (event) => {
+   event.preventDefault()
+   
+   this.setState({
+     username: '',
+     password: ''
+   })
+ }
+
   render(){
-    const handleOnSubmit = (event) => {
-      event.preventDefault()
-    
-    }
     return(
       <div>
         <h1>Welcome to GuitarLists</h1>
         <div>
           <h2>Sign Up</h2>
-          <form>
-            <label name="username">Username</label>
-            <input type="text" name="usernmame" />
+          <form onSubmit={this.handleOnSubmit}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" value={ this.state.username } onChange={this.handleOnChange}/>
 
-            <label name="password">Password</label>
-            <input type="password" name="password" />
+            <label htmlFor="password">Password</label>
+            <input type="text" name="password" value={ this.state.password } onChange={this.handleOnChange}/>
 
-            <input type="submit" onSubmit={handleOnSubmit}/>
+            <input type="submit" />
           </form>
         </div>
       </div>
