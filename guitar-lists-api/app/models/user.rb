55ea9a91855a2aceb50620774eb2f0eff7c9af71
Rafael <RafaelB24@yahoo.com>
2019-09-20 22:44:10 -0400

    Add username to User model, add spec to test for username

--- guitar-lists-api/app/models/user.rb
+++ guitar-lists-api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
   # when using #sellers_params in SellersController #update, update will return false. Passing 'if' with a proc, validate password length if
   #  the seller is a new record or the password is not blank. If it is blank it will still return true when updating but password will not be changed
   validates :password, length: { minimum: 5 }, if: Proc.new{ |obj| obj.new_record? || !obj.password.blank? }
--- guitar-lists-api/spec/models/users_spec.rb
+++ guitar-lists-api/spec/models/users_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     # Shoulda-Matcher validations, see the docs for more info
     # https://github.com/thoughtbot/shoulda-matchers
     it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password) }
     it { should have_secure_password }
     it { should have_many(:guitars) }
