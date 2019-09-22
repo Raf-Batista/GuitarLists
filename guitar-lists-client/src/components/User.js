e01e436333f308d02044ed2aa609858077622fad
Rafael <RafaelB24@yahoo.com>
2019-09-22 19:35:06 -0400

    Guitars component renders all users with link to user show page

--- guitar-lists-client/src/components/User.js
+++ guitar-lists-client/src/components/User.js
@@ -5,7 +5,7 @@ class User extends Component {
   render(){
     return(
       <div>
-
+        <p>{this.props.user.username}</p>
       </div>
     )
   }
--- guitar-lists-client/src/components/Users.js
+++ guitar-lists-client/src/components/Users.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import User from '../components/User';
 
 class Users extends Component {
 
   render(){
+    let userUrl;
     return(
       <div id="users">
-
+        {this.props.users.map(user => {
+          userUrl=`users/${user.id}`
+          return <NavLink key={user.id} exact activeClassName = 'active-link' className = 'home navbar-brand' to={userUrl}>{user.username}</NavLink>
+        })}
       </div>
     )
   }
