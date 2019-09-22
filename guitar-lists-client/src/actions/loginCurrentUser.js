3b34f5bacf7bf54d866087a67e2e37735f72a61b
Rafael <RafaelB24@yahoo.com>
2019-09-21 23:33:26 -0400

    loginCurrentUser will send request to backend if there is a token

--- guitar-lists-client/src/actions/loginCurrentUser.js
+++ guitar-lists-client/src/actions/loginCurrentUser.js
@@ -1,21 +1,24 @@
 const loginCurrentUser = () => {
   return dispatch => {
-    return fetch('http://localhost:3000/session', {
-      method: 'POST',
-      body: JSON.stringify({token: localStorage.getItem('token')}),
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    }).then(response => response.json())
-      .then(data => {
-        if(data.email && data.username && data.id){
-          dispatch({type: 'LOGIN', payload: data})
-        } else {
-          return data.errors
+    if(localStorage.getItem('token')){
+      return fetch('http://localhost:3000/session', {
+        method: 'POST',
+        body: JSON.stringify({token: localStorage.getItem('token')}),
+        headers:{
+          'Content-Type': 'application/json'
         }
+      }).then(response => response.json())
+        .then(data => {
+          if(data.email && data.username && data.id){
+            dispatch({type: 'LOGIN', payload: data})
+          } else {
+            return data.errors
+          }
 
-      })
-    .catch(error => console.log(error))
+        })
+      .catch(error => console.log(error))
+    }
+
   }
 }
 
