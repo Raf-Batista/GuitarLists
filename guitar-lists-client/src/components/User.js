258d30c37e3d218e12466bc1816dbc7244fce67a
Rafael <RafaelB24@yahoo.com>
2019-09-23 13:12:43 -0400

    Add componentDidMount to User.js to fetch user data

--- guitar-lists-client/src/components/User.js
+++ guitar-lists-client/src/components/User.js
@@ -2,28 +2,48 @@ import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom'
 
 class User extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {user: ''}
+  }
+  componentDidMount(){
+    if(!this.props.location.state) {
+      const id = this.props.match.params.id
+      fetch(`http://localhost:3000/users/${id}`)
+      .then(response => response.json())
+      .then(user => {
+        this.setState({
+          user: user
+        })
+      })
+    } else {
+      this.setState({
+        user: this.props.location.state.user
+      })
+    }
+  }
 
   render(){
-    const user = this.props.location.state.user
 
     return(
       <div>
-        <p>{user.username}</p>
-        {user.guitars.map(guitar => {
-          const url = `/users/${guitar.user_id}/guitars/${guitar.id}`
-          return <div>
-            <NavLink
-              key={guitar.id}
-              activeClassName = 'active-link'
-              className = 'home navbar-brand'
-              exact
-              to={{pathname: url, state: {guitar: guitar}}}>
-              {guitar.model}  ${guitar.price}
-            </NavLink>
+      <p>{this.state.user.username}</p>
+      {/* render can run before component did mount, the conditional prevents Typerror undefined */}
+      {this.state.user.guitars && this.state.user.guitars.map(guitar => {
+        const url = `/users/${guitar.user_id}/guitars/${guitar.id}`
+        return <div>
+          <NavLink
+            key={guitar.id}
+            activeClassName = 'active-link'
+            className = 'home navbar-brand'
+            exact
+            to={{pathname: url, state: {guitar: guitar}}}>
+            {guitar.model}  ${guitar.price}
+          </NavLink>
 
 
-          </div>
-        })}
+        </div>
+      })}
       </div>
     )
   }
