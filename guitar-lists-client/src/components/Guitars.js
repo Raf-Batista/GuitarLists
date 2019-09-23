d46929cc3193fffc4518d5cb3a298d900ed882f0
Rafael <RafaelB24@yahoo.com>
2019-09-22 22:51:42 -0400

    Add search bar to filter guitars in guitars index

--- guitar-lists-client/src/components/Guitars.js
+++ guitar-lists-client/src/components/Guitars.js
@@ -8,9 +8,16 @@ class Guitars extends Component {
     return(
       <div id="guitars">
         {this.props.guitars.map(guitar => {
-          url=`guitars/${guitar.id}`
-          return <NavLink key={guitar.id} exact activeClassName = 'active-link' className = 'home navbar-brand' to={{pathname: url, state: {guitar: guitar}}}>{guitar.model}</NavLink>
-        })}
+            url=`guitars/${guitar.id}`
+            return <NavLink key={guitar.id} exact 
+            activeClassName = 'active-link'
+            className = 'home navbar-brand'
+            to={{pathname: url, state: {guitar: guitar}}}>
+            {guitar.model}
+            </NavLink>
+          })
+        }
+
       </div>
     )
   }
--- guitar-lists-client/src/containers/GuitarsContainer.js
+++ guitar-lists-client/src/containers/GuitarsContainer.js
@@ -4,11 +4,30 @@ import Guitars from '../components/Guitars'
 class GuitarsContainer extends Component {
   constructor(props) {
     super(props)
+    this.state = {query: '', guitars: this.props.guitars}
   }
 
+  handleOnChange = (event) => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+
   render(){
+    const {query} = this.state;
+    const lowercasedQuery = query.toLowerCase();
+    const filteredGuitars = this.props.guitars.filter(guitar => {
+      if(guitar.model.includes(lowercasedQuery)) {
+        return guitar
+      }
+    })
     return(
-      <Guitars guitars={this.props.guitars} />
+      <div>
+        <label>Search</label>
+        <input type="text" name="search" onChange={this.handleOnChange} value={this.state.query}/>
+        <Guitars guitars={filteredGuitars}/>
+      </div>
     )
   }
 }
