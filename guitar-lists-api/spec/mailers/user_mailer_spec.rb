2a94f2596c7fb9bcbc8b705b05f63cd88440f5bf
Rafael <RafaelB24@yahoo.com>
2019-09-28 14:48:42 -0700

    Add test to see if correct subject is sent in welcome email

--- guitar-lists-api/spec/mailers/user_mailer_spec.rb
+++ guitar-lists-api/spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:context) do
+    User.create(email: 'test@example.com', username: 'test', password: 'test123')
+  end
+
+  describe "welcome" do
+    it "has the correct subject in the mail" do
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.subject).to eq('Welcome to GuitarLists')
+    end
+  end
+
 end
