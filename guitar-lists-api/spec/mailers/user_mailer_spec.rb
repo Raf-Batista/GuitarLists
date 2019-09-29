f55313cfb6cfdfdb1c817aa8589d16d4d4a2a7fe
Rafael <RafaelB24@yahoo.com>
2019-09-28 22:28:43 -0700

    Add test to see if email is sent from GuitarLists

--- guitar-lists-api/spec/mailers/user_mailer_spec.rb
+++ guitar-lists-api/spec/mailers/user_mailer_spec.rb
@@ -3,12 +3,27 @@ require "rails_helper"
 RSpec.describe UserMailer, type: :mailer do
 
   describe "welcome" do
+
     it "has the correct subject in the mail" do
       user = User.create(email: 'test@example.com', username: 'test', password: 'test123')
       UserMailer.welcome(user).deliver_now
       mail = ActionMailer::Base.deliveries.last
       expect(mail.subject).to eq('Welcome to GuitarLists')
     end
-  end
 
+    it "is sent to the newly creted user" do
+      user = User.create(email: 'test@example.com', username: 'test', password: 'test123')
+      UserMailer.welcome(user).deliver_now
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.to[0]).to eq(user.email)
+    end
+
+    it "is sent from GuitarLists" do
+      user = User.create(email: 'test@example.com', username: 'test', password: 'test123')
+      UserMailer.welcome(user).deliver_now
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.from[0]).to eq("#{ENV["GUITARLISTS_USERNAME"]}@gmail.com")
+    end
+
+  end
 end
