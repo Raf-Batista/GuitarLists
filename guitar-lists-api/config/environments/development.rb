0699b660a4b041cb612f1ad9c9a992317a45bc83
Rafael <RafaelB24@yahoo.com>
2019-10-05 15:20:37 -0700

    Fix spacing in config files

--- guitar-lists-api/config/environments/development.rb
+++ guitar-lists-api/config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-    config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
