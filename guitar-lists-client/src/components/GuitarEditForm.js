d3d7c9f307a95bca889959d0ed4c8e0477613e6c
Rafael <RafaelB24@yahoo.com>
2019-10-07 15:19:15 -0700

    Add value from state  to each input in form

--- guitar-lists-client/src/components/GuitarEditForm.js
+++ guitar-lists-client/src/components/GuitarEditForm.js
@@ -21,22 +21,22 @@ class GuitarEditForm extends Component {
             {this.props.currentUser.username ?  
             <form>
                 <label htmlFor="model" name="model">Model:</label>
-                <input type="text" name="model" />
+                <input type="text" name="model" value={this.state.guitar.model}/>
 
                 <label htmlFor="spec" name="spec">Spec:</label>
-                <input type="text" name="spec" />
+                <input type="text" name="spec" value={this.state.guitar.spec}/>
 
 
                 <label htmlFor="price" name="price">Price:</label>
-                <input type="text" name="price" />
+                <input type="text" name="price" value={this.state.guitar.price}/>
 
 
                 <label htmlFor="condition" name="condition">Condition:</label>
-                <input type="text" name="condition" />
+                <input type="text" name="condition" value={this.state.guitar.condition}/>
 
                  
                 <label htmlFor="location" name="location">Location:</label>
-                <input type="text" name="location" />
+                <input type="text" name="location" value={this.state.guitar.location}/>
 
                 <button type="submit">Edit Guitar</button>
             </form> :
