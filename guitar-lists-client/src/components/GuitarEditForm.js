354ce50e9ad9c9cf42e8e95ec79e49557a0dd3fe
Rafael <RafaelB24@yahoo.com>
2019-10-07 15:11:19 -0700

    Add form to GuitarEditForm

--- guitar-lists-client/src/components/GuitarEditForm.js
+++ guitar-lists-client/src/components/GuitarEditForm.js
@@ -4,13 +4,13 @@ import { withRouter, Redirect } from 'react-router-dom';
 class GuitarEditForm extends Component {
     constructor(props) {
         super(props)
+        this.state = {guitar: ''}
     }
     componentDidMount() {
         const {userId, guitarId} = this.props.match.params
         this.props.guitars.find(guitar => {
             if(guitar.user_id === parseInt(userId) && guitar.id === parseInt(guitarId)) { 
-                console.log(guitar)
-                return guitar
+                this.setState({guitar: guitar})
               }
         })
     }
@@ -19,7 +19,27 @@ class GuitarEditForm extends Component {
         return(
             <div>
             {this.props.currentUser.username ?  
-            <p>GuitarEditForm</p> :
+            <form>
+                <label htmlFor="model" name="model">Model:</label>
+                <input type="text" name="model" />
+
+                <label htmlFor="spec" name="spec">Spec:</label>
+                <input type="text" name="spec" />
+
+
+                <label htmlFor="price" name="price">Price:</label>
+                <input type="text" name="price" />
+
+
+                <label htmlFor="condition" name="condition">Condition:</label>
+                <input type="text" name="condition" />
+
+                 
+                <label htmlFor="location" name="location">Location:</label>
+                <input type="text" name="location" />
+
+                <button type="submit">Edit Guitar</button>
+            </form> :
                <Redirect to='/' />
             }
        </div>
