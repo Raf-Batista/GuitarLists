5712d036d972206f96ac52ccee900316e4aea663
Rafael <RafaelB24@yahoo.com>
2019-10-10 13:47:18 -0700

    Add reducers test to GuitarsReducer.test.js

--- guitar-lists-client/__tests__/GuitarsReducer.test.js
+++ guitar-lists-client/__tests__/GuitarsReducer.test.js
@@ -4,4 +4,16 @@ describe('Guitars Reducer', () => {
     it('should return the initial state', () => {
         expect(reducer(undefined, {})).toEqual([])
     })
+
+    it('should add a guitar', () => {
+        expect(reducer([], {type: 'ADD_GUITAR', payload: {model: 'test'}})).toEqual([{model: 'test'}])
+    })
+
+    it('should edit a guitar', () => {
+        expect(reducer([{id: 1, model: 'before'}], {type: 'EDIT_GUITAR', payload: {id: 1, model: 'after'}})).toEqual([{id: 1, model: 'after'}])
+    })
+
+    it('should delete a guitar', () => {
+        expect(reducer([{id: 1, model: 'delete'}], {type: 'DELETE_GUITAR', payload: {guitarId: 1}})).toEqual([])
+    })
 })
