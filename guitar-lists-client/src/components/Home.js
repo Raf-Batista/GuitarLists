51c9f35e60358427d512160fc2995643e6d88c8f
Rafael <RafaelB24@yahoo.com>
2019-10-09 18:07:42 -0700

    Add margin top to div#home

--- guitar-lists-client/src/components/Home.js
+++ guitar-lists-client/src/components/Home.js
@@ -6,7 +6,8 @@ import { connect } from 'react-redux';
 class Home extends Component {
   render(){
     return(
-      <div id="home">
+      <div className="container">
+          <div id="home" className="mt-3">
         {
           this.props.currentUser.username ? // if a user is found in redux state, redirect them to User's show page, else render signup form
           <Redirect to={{pathname: `/users/${this.props.currentUser.id}`}} /> :
@@ -15,6 +16,7 @@ class Home extends Component {
             <Signup signup={this.props.signup}/>
           </div>
         }
+      </div>
       </div>
     )
   }
