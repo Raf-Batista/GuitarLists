7deb00b22b5bcbad76136a7e602e984e053a3843
Rafael <RafaelB24@yahoo.com>
2019-10-18 16:49:32 -0700

    Remove state as a prop to NavLink

--- guitar-lists-client/src/components/Guitars.js
+++ guitar-lists-client/src/components/Guitars.js
@@ -4,17 +4,15 @@ import { NavLink } from 'react-router-dom';
 class Guitars extends Component {
 
   render(){
-    let url;
     return(
       <div className="container" id="guitars">
         {this.props.guitars.map(guitar => {
-            url=`/users/${guitar.user_id}/guitars/${guitar.id}`
             return <NavLink
               key={guitar.id} 
               exact
               activeClassName = 'active-link'
               className = 'home navbar-brand d-block'
-              to={{pathname: url, state: {guitar: guitar}}}>
+              to={`/users/${guitar.user_id}/guitars/${guitar.id}`}>
               {guitar.model}
             </NavLink>
           })
