733fadf2857f621725183fff88da7e5aad5b0f5f
Rafael <RafaelB24@yahoo.com>
2019-10-27 13:59:39 -0700

    Set up Postgresql

--- guitar-lists-api/db/seeds.rb
+++ guitar-lists-api/db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-user1 = User.create(email: 'test1@email.com', username: 'test1', password: 'password')
-user1.guitars.build(model: 'test_model1', price: 100, condition: 'new', location: 'no_where', spec: 'test_spec').save
-
-user2 = User.create(email: 'test2@email.com', username: 'test2', password: 'password')
-user2.guitars.build(model: 'test_model2', price: 200, condition: 'new', location: 'no where', spec: 'test_spec').save
